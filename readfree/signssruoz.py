4c318bea7b812a02d91367ab8a8cc19bb9ed6ca6
ssrzz <ssrzz@pm.me>
2019-02-22 20:12:58 +0800

    [bug fixed]: forget to import certain package for suorz

--- readfree/signssruoz.py
+++ readfree/signssruoz.py
@@ -2,19 +2,21 @@
 from rfv2 import ReadFree
 import pprint
 import arrow
-import sys 
+import sys
+import time 
 
 # Time printing for debugging purpose 
 # To see when the program went wrong.
 # choice = sys.argv[1]
 
 while True:
-	rf = ReadFree('/usr/local/info/ssruoz_rf.dat', '/usr/local/info/ssruoz_rf.json')    	
-	rf.login()
-	print(arrow.now().format('YYYY-MM-DD HH-mm'))
-	pprint.pprint(rf.getAccountInfo())
-	break
-	time.sleep(3)
+	try:
+		rf = ReadFree('/usr/local/info/ssruoz_rf.dat', '/usr/local/info/ssruoz_rf.json')    	
+		print(arrow.now().format('YYYY-MM-DD HH-mm'))
+		pprint.pprint(rf.getAccountInfo())
+	except Exception as e:
+		print(e)
+	time.sleep(3600)
 
 
 
