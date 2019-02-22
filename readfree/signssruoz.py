fa7f67500462b391757052c3ffb1c94a2bef8c56
ssrzz <ssrzz@pm.me>
2019-02-22 20:05:03 +0800

    [update] bot for readfree ssruoz

--- readfree/signssruoz.py
+++ readfree/signssruoz.py
@@ -6,15 +6,11 @@ import sys
 
 # Time printing for debugging purpose 
 # To see when the program went wrong.
-choice = sys.argv[1]
+# choice = sys.argv[1]
 
 while True:
-	rf = ''
-	if int(choice) == 1:
-		rf = ReadFree('/usr/local/info/rfcookies.pkl', '/usr/local/info/readfree.json')
-	else:
-		rf = ReadFree('/usr/local/info/ssruoz_rf.dat', '/usr/local/info/ssruoz_rf.json')    
-
+	rf = ReadFree('/usr/local/info/ssruoz_rf.dat', '/usr/local/info/ssruoz_rf.json')    	
+	rf.login()
 	print(arrow.now().format('YYYY-MM-DD HH-mm'))
 	pprint.pprint(rf.getAccountInfo())
 	break
